82df9f8049ccbe0cf25e7a880569a6efb611e9a6
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-04 21:52:30 +0700

    Add project files.

--- src/Fathcore/Abstractions/IScopedService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fathcore.Abstractions
-{
-    /// <summary>
-    /// Objects which derived from this interface will be registered as scoped service in dependency injection container
-    /// </summary>
-    public interface IScopedService
-    {
-    }
-}
--- src/Fathcore/Abstractions/ISingletonService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fathcore.Abstractions
-{
-    /// <summary>
-    /// Objects which derived from this interface will be registered as singleton service in dependency injection container
-    /// </summary>
-    public interface ISingletonService
-    {
-    }
-}
