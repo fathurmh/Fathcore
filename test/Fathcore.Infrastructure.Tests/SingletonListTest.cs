e2967b09f61e9cb1f1c96c38d4f404fb9ec2da62
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-08 22:22:24 +0700

    Add infrastructure.

---
+++ test/Fathcore.Infrastructure.Tests/SingletonListTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Fathcore.Infrastructure.Tests
+{
+    public class SingletonListTest
+    {
+        [Fact]
+        public void SingletonList_Is_Created_By_Default()
+        {
+            IList<SingletonListTest> instance = SingletonList<SingletonListTest>.Instance;
+
+            Assert.NotNull(instance);
+        }
+
+        [Fact]
+        public void SingletonList_Can_Store_Items()
+        {
+            IList<SingletonListTest> instance = SingletonList<SingletonListTest>.Instance;
+            instance.Insert(0, this);
+
+            Assert.Same(this, instance[0]);
+        }
+    }
+}
---
+++ test/Fathcore.Infrastructure.Tests/SingletonTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Fathcore.Infrastructure.Tests
+{
+    public class SingletonTest
+    {
+        [Fact]
+        public void Singleton_Is_Null_By_Default()
+        {
+            SingletonTest instance = Singleton<SingletonTest>.Instance;
+
+            Assert.Null(instance);
+        }
+
+        [Fact]
+        public void Singleton_Share_Same_Singletons_Dictionary()
+        {
+            Singleton<int>.Instance = 1;
+            Singleton<double>.Instance = 2.0;
+
+            Assert.Equal(Singleton<int>.AllSingletons, Singleton<double>.AllSingletons);
+            Assert.Equal(1, BaseSingleton.AllSingletons[typeof(int)]);
+            Assert.Equal(2.0, BaseSingleton.AllSingletons[typeof(double)]);
+        }
+    }
+}
