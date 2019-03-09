5f9e82f7fd845c4d2ddf7064cc2623beb246a22c
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-09 22:45:00 +0700

    Update test script.

--- test.cmd
+++ test.cmd
@@ -3,6 +3,6 @@ dotnet test
 
 which coverlet || dotnet tool install --global coverlet.console
 
-coverlet ./artifacts/bin/Fathcore.Tests/Debug/netcoreapp2.2/Fathcore.Tests.dll --target "dotnet" --targetargs "test --no-build" --exclude "[xunit*]*" --format lcov --format opencover --output ./artifacts/TestResults/
+coverlet ./artifacts/bin/Fathcore.Tests/Debug/netcoreapp2.2/Fathcore.Tests.dll --target "dotnet" --targetargs "test --no-build" --exclude "[xunit*]*" --output ./artifacts/TestResults/ --format lcov --format opencover 
 
-coverlet ./artifacts/bin/Fathcore.Extensions.Tests/Debug/netcoreapp2.2/Fathcore.Extensions.Tests.dll --target "dotnet" --targetargs "test --no-build" --exclude "[xunit*]*" --format lcov --format opencover --merge-with "coverage.json" --output ./artifacts/TestResults/
+coverlet ./artifacts/bin/Fathcore.Extensions.Tests/Debug/netcoreapp2.2/Fathcore.Extensions.Tests.dll --target "dotnet" --targetargs "test --no-build" --exclude "[xunit*]*" --merge-with "./artifacts/TestResults/coverage.json" --output ./artifacts/TestResults/ --format lcov --format opencover 
