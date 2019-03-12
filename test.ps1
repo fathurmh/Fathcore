49916351d4d7e3f4b459058830f8131b00dcfd8e
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-12 18:08:50 +0700

    Update some configuration.

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@ dotnet test `
   ./${workspaceFolder}/test/Fathcore.Tests/Fathcore.Tests.csproj `
   --no-build `
   /p:CollectCoverage=true `
-  /p:CoverletOutputFormat=opencover `
+  /p:CoverletOutputFormat="json%2copencover" `
   /p:CoverletOutput=$PSScriptRoot/artifacts/TestResults/ `
   /p:Exclude="[xunit*]*%2c[*.Tests?]*"
 
@@ -10,7 +10,9 @@ dotnet test `
   ./${workspaceFolder}/test/Fathcore.Extensions.Tests/Fathcore.Extensions.Tests.csproj `
   --no-build `
   /p:CollectCoverage=true `
-  /p:CoverletOutputFormat=opencover `
+  /p:CoverletOutputFormat="json%2copencover" `
   /p:CoverletOutput=$PSScriptRoot/artifacts/TestResults/ `
   /p:Exclude="[xunit*]*%2c[*.Tests?]*" `
   /p:MergeWith=${PSScriptRoot}/artifacts/TestResults/coverage.json
+
+Remove-Item ${PSScriptRoot}/artifacts/TestResults/coverage.json
