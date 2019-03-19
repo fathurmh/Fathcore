80c7a496eff9eafd2ef6057ef32776d19883b933
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-19 22:20:17 +0700

    Add etag factory.

---
+++ src/Fathcore/EntityTag/EtagFactoryServicesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Fathcore.EntityTag;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fathcore.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for EtagFactory.
+    /// </summary>
+    public static class EtagFactoryServicesExtensions
+    {
+        /// <summary>
+        /// Add etag factory service.
+        /// to the specified <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddEtagFactory(this IServiceCollection services)
+        {
+            services.AddScoped<EtagFactory>();
+            services.AddScoped<IEtagFactory>(provider => provider.GetRequiredService<EtagFactory>());
+
+            return services;
+        }
+    }
+}
