1af2a40e433e9a053504e96cf5a9c644b17160bf
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-19 12:06:05 +0700

    Add fluent validation and default localization provider.

---
+++ src/Fathcore/Localization/ICultureInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Fathcore.Localization
+{
+    /// <summary>
+    /// Represents culture info.
+    /// </summary>
+    public interface ICultureInfo
+    {
+        /// <summary>
+        /// Gets a default culture info.
+        /// </summary>
+        string Default { get; }
+
+        /// <summary>
+        /// Gets a default for UI culture info.
+        /// </summary>
+        string DefaultUI { get; }
+    }
+}
