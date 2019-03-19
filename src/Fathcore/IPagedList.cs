03c408363d51dff1cdc72b7a1b164d795dc0de7e
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-19 23:57:24 +0700

    Add paged list.

---
+++ src/Fathcore/IPagedList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Fathcore
+{
+    /// <summary>
+    /// Represents a strongly typed paged list of objects that can be accessed by index. Provides methods to search, sort, and manipulate lists.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    public interface IPagedList<T> : ICollection<T>, IEnumerable<T>, IList<T>, IReadOnlyCollection<T>, IReadOnlyList<T>, ICollection, IEnumerable, IList
+    {
+        /// <summary>
+        /// Page index.
+        /// </summary>
+        int PageIndex { get; }
+
+        /// <summary>
+        /// Page size.
+        /// </summary>
+        int PageSize { get; }
+
+        /// <summary>
+        /// Total count.
+        /// </summary>
+        int TotalCount { get; }
+
+        /// <summary>
+        /// Total pages.
+        /// </summary>
+        int TotalPages { get; }
+
+        /// <summary>
+        /// Has previous page.
+        /// </summary>
+        bool HasPreviousPage { get; }
+
+        /// <summary>
+        /// Has next page.
+        /// </summary>
+        bool HasNextPage { get; }
+    }
+}
---
+++ src/Fathcore/PagedList.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fathcore
+{
+    /// <summary>
+    /// Represents a strongly typed paged list of objects that can be accessed by index. Provides methods to search, sort, and manipulate lists.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    public class PagedList<T> : List<T>, IPagedList<T>
+    {
+        /// <summary>
+        /// Page index.
+        /// </summary>
+        public int PageIndex { get; }
+
+        /// <summary>
+        /// Page size.
+        /// </summary>
+        public int PageSize { get; }
+
+        /// <summary>
+        /// Total count.
+        /// </summary>
+        public int TotalCount { get; }
+
+        /// <summary>
+        /// Total pages.
+        /// </summary>
+        public int TotalPages { get; }
+
+        /// <summary>
+        /// Has previous page.
+        /// </summary>
+        public bool HasPreviousPage => PageIndex > 0;
+
+        /// <summary>
+        /// Has next page.
+        /// </summary>
+        public bool HasNextPage => PageIndex + 1 < TotalPages;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PagedList{T}"/> class that contains elements copied from the specified collection and has sufficient capacity to accommodate the number of elements copied.
+        /// </summary>
+        /// <param name="source">The collection whose elements are copied to the new list.</param>
+        /// <param name="pageIndex">Page index.</param>
+        /// <param name="pageSize">Page size.</param>
+        /// <param name="getOnlyTotalCount">A value in indicating whether you want to load only total number of records. Set to "true" if you don't want to load data from database.</param>
+        public PagedList(IQueryable<T> source, int pageIndex, int pageSize, bool getOnlyTotalCount = false)
+        {
+            var total = source.Count();
+            TotalCount = total;
+            TotalPages = total / pageSize;
+
+            if (total % pageSize > 0)
+                TotalPages++;
+
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+
+            if (getOnlyTotalCount)
+                return;
+
+            AddRange(source.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PagedList{T}"/> class that contains elements copied from the specified collection and has sufficient capacity to accommodate the number of elements copied.
+        /// </summary>
+        /// <param name="source">The collection whose elements are copied to the new list.</param>
+        /// <param name="pageIndex">Page index.</param>
+        /// <param name="pageSize">Page size.</param>
+        public PagedList(IList<T> source, int pageIndex, int pageSize)
+        {
+            TotalCount = source.Count;
+            TotalPages = TotalCount / pageSize;
+
+            if (TotalCount % pageSize > 0)
+                TotalPages++;
+
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+
+            AddRange(source.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PagedList{T}"/> class that contains elements copied from the specified collection and has sufficient capacity to accommodate the number of elements copied.
+        /// </summary>
+        /// <param name="source">The collection whose elements are copied to the new list.</param>
+        /// <param name="pageIndex">Page index.</param>
+        /// <param name="pageSize">Page size.</param>
+        public PagedList(IEnumerable<T> source, int pageIndex, int pageSize)
+        {
+            TotalCount = source.Count();
+            TotalPages = TotalCount / pageSize;
+
+            if (TotalCount % pageSize > 0)
+                TotalPages++;
+
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+
+            AddRange(source.Skip(pageIndex * pageSize).Take(pageSize).ToList());
+        }
+    }
+}
