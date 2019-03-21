d1040182be5034e78de12322a58a3c494f6f16cd
Fathurahman Ma'ruf <fathur.mh@gmail.com>
2019-03-21 16:39:48 +0700

    Add engine test.

---
+++ test/Fathcore.Tests/EngineTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Fathcore.Infrastructure;
+using Xunit;
+
+namespace Fathcore.Tests
+{
+    public class EngineTest : IDisposable
+    {
+        [Fact]
+        public void Engine_IsCreated_ByDefault()
+        {
+            Singleton<IEngine>.Instance = null;
+
+            Assert.Null(Singleton<IEngine>.Instance);
+            Assert.Same(Engine.Current, Singleton<IEngine>.Instance);
+            Assert.NotNull(Singleton<IEngine>.Instance);
+        }
+
+        [Fact]
+        public void Engine_CreateAnInstance_ByItSelf()
+        {
+            var engine = Engine.Create();
+
+            Assert.Same(engine, BaseSingleton.AllSingletons[typeof(IEngine)]);
+            Assert.Same(engine, Engine.Current);
+            Assert.Same(Singleton<IEngine>.Instance, Engine.Current);
+        }
+
+        [Fact]
+        public void Engine_ShouldReplace_AnInstance()
+        {
+            var engine = Engine.Create();
+            var newEngine = new Infrastructure.Engine();
+
+            Assert.Same(engine, Engine.Current);
+            Assert.NotSame(newEngine, Engine.Current);
+
+            Engine.Replace(newEngine);
+
+            Assert.Same(newEngine, Engine.Current);
+        }
+
+        public void Dispose()
+        {
+            Engine.Replace(new Infrastructure.Engine());
+        }
+    }
+}
